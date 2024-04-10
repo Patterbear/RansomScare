3578c3466605ce90fe33f51ba8639987ffc5f2ff
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-04-10 20:00:19 +0100

    add basic splash window with label

---
+++ RansomScare/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information
+[assembly: AssemblyTitle("RansomScare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RansomScare")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// makes the types in this assembly not visible to COM components
+[assembly: ComVisible(false)]
+
+// GUID for ID of the typelib if project exposed to COM
+[assembly: Guid("e072bedf-5f33-4609-a080-e7d94bae1338")]
+
+// Version information for assembly
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RansomScare/SplashScreen.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RansomScare
+{
+    partial class SplashScreen
+    {
+        // Required designer variable.
+        private System.ComponentModel.IContainer components = null;
+
+        // Clean up any resources being used.
+        // 'disposing' determines if managing resources should be disposed of
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.welcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcome
+            // 
+            this.welcome.AutoSize = true;
+            this.welcome.Font = new System.Drawing.Font("Segoe UI Semibold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcome.Location = new System.Drawing.Point(117, 9);
+            this.welcome.Name = "welcome";
+            this.welcome.Size = new System.Drawing.Size(599, 65);
+            this.welcome.TabIndex = 0;
+            this.welcome.Text = "Welcome to RansomScare";
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcome);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SplashScreen";
+            this.Text = "RansomScare - Welcome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcome;
+    }
+}
+
