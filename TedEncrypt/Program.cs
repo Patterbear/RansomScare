20961bab3bd933a3849a3798da20c95a341fd21c
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-04-14 20:33:08 +0100

    add comments to main class

--- TedEncrypt/Program.cs
+++ TedEncrypt/Program.cs
@@ -21,24 +21,29 @@ namespace TedEncrypt
             
         }
 
+        // Check TedEncrypt status
+        // check to see if folder has been previously encrypted or if new key is needed
+        // uses hidden .tedencrypt file
         static void CheckTedEncryptStatus()
         {
             string statusFile = Directory.GetCurrentDirectory() + "/.tedencrypt";
 
             if (File.Exists(statusFile))
             {
+                // Files are encrypted
                 if (File.ReadAllText(statusFile).Equals("e"))
                 {
                     Application.Run(new EncryptedScreen());
                 }
+                // Files are decrypted
                 else if (File.ReadAllText(statusFile).Equals("d"))
                 {
                     Application.Run(new DecryptedScreen());
-                } else
+                } else // file exists but is invalid
                 {
                     Application.Run(new SplashScreen());
                 }
-            } else
+            } else // file does not exist
             {
                 Application.Run(new SplashScreen());
             }
