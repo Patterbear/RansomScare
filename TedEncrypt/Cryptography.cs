fe84538631ea79886e635de117f1d3767979ea05
Patterbear <benjamin.mcgregor2002@gmail.com>
2024-04-16 08:54:06 +0100

    remove unused imports

--- TedEncrypt/Cryptography.cs
+++ TedEncrypt/Cryptography.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace TedEncrypt
 {
